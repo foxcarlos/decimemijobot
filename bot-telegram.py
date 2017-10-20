03c55dd6098f5cd989665974b519d1bc3390d621
foxcarlos <foxcarlos@gmail.com>
2017-10-20 08:47:23 -0300

    [DEL] se elimino comandos en desuso

--- bot-telegram.py
+++ bot-telegram.py
@@ -38,22 +38,6 @@ def comando_calculo(mensaje):
     user_first_name = mensaje.from_user.first_name
     bot.send_message(chat_id, response.format(user_first_name, total_sum))
 
-@bot.message_handler(commands=['suma'])
-def comando_suma(mensaje):
-    '''.'''
-
-    chat_id = mensaje.chat.id
-    try:
-        parameters = mensaje.text.split()[1].split(',')
-        total_sum = sum([float(n) for n in parameters])
-    except Exception as inst:
-        total_sum = 0
-
-    user_first_name = mensaje.from_user.first_name
-
-    response = 'Ey..! {0} aqui teneis tu suma, {1}, a ver si te comprais una calculadora'
-    bot.send_message(chat_id, response.format(user_first_name, total_sum))
-
 @bot.message_handler(commands=['quierocolaborar'])
 def comando_quierocolaborar(mensaje):
     '''.'''
@@ -89,13 +73,11 @@ def ayuda(mensaje):
     chat_id = mensaje.chat.id
     msg_response = """
     Lista de Comandos:
-    /suma 2,2,5
     /calcular (2+4) * 5
     /hola
     /chao
     /ayudame
     /help
-    /dameunaayudaitaahi
     /quierocolaborar
     /dolartoday
     /bitcoin
@@ -149,7 +131,7 @@ def comando_porno(mensaje):
 
     chat_id = mensaje.chat.id
     msg_response = """
-     Aqui teneis  esto pa' que te pajieis: https://www.redtube.com/redtube/gay
+     Aqui teneis  esto pervertido: https://www.redtube.com/redtube/gay
     """
     user_first_name = mensaje.from_user.first_name
 
