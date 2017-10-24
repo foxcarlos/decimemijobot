d7596552e81156d133891501f3e6a4d228e40fa5
foxcarlos <foxcarlos@gmail.com>
2017-10-24 15:33:17 -0300

    [ADD] comando para noticias de Panorama

--- bot-telegram.py
+++ bot-telegram.py
@@ -2,23 +2,64 @@
 import telebot
 from telebot import types
 import requests
+from scrapy import NoticiasPanorama
 
 
 TOKEN = '336382255:AAHwrdIgN0j3gIet0xnJfCKs78ojp1dm28s'
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(commands=['hola'])
-def comando_hola(mensaje):
+@bot.message_handler(commands=['ayudame'])
+def ayuda(mensaje):
+    response = ayuda(mensaje)
+
+@bot.message_handler(commands=['help'])
+def ayuda(mensaje):
+    response = ayuda(mensaje)
+
+@bot.message_handler(commands=['?'])
+def ayuda(mensaje):
+    response = ayuda(mensaje)
+
+@bot.message_handler(commands=['dameunaayudaitaahi'])
+def ayuda(mensaje):
+    response = ayuda(mensaje)
+
+def ayuda(mensaje):
     '''.'''
 
-    print(mensaje)
+    chat_id = mensaje.chat.id
+    msg_response = """
+    Lista de Comandos:
+
+    /bitcoin
+    /calcular  La suma de 2 mas  es [2+2] y 3 por 3 es [3*3]
+    /chao
+    /dolartoday
+    /help
+    /hola
+    /panorama
+    /porno
+    """
+    user_first_name = mensaje.from_user.first_name
+
+    response = 'Ey..! {0} aqui teneis tu ayuda, {1}'
+    bot.send_message(chat_id, response.format(user_first_name, msg_response))
+
+@bot.message_handler(commands=['bitcoin'])
+def bitcoin(mensaje):
+    '''.'''
+
     chat_id = mensaje.chat.id
     user_first_name = mensaje.from_user.first_name
 
-    bot.send_message(chat_id, 'Que fue mijo {0} como estais..?'.format(user_first_name))
+    url = "https://api.coinbase.com/v2/exchange-rates?currency=BTC"
+    response = requests.get(url).json().get("data").get("rates").get("USD")
+
+    msg_response = '{0} El precio del Bitcoin es: {1} USD'.format(user_first_name, response)
+    bot.send_message(chat_id, msg_response)
 
 @bot.message_handler(commands=['calcular'])
-def comando_calculo(mensaje):
+def calculo(mensaje):
     '''.'''
 
     parameters = mensaje.text
@@ -42,6 +83,37 @@ def comando_calculo(mensaje):
 
     bot.send_message(chat_id, response)
 
+@bot.message_handler(commands=['chao'])
+def chao(mensaje):
+    '''.'''
+
+    chat_id = mensaje.chat.id
+    bot.send_message(chat_id, 'Dala papi nos vemos, cualquier verga gritais')
+
+@bot.message_handler(commands=['dolartoday'])
+def dolartoday(mensaje):
+    '''.'''
+
+    chat_id = mensaje.chat.id
+    user_first_name = mensaje.from_user.first_name
+
+    rq = requests.get('https://s3.amazonaws.com/dolartoday/data.json')
+    devuelto = rq.json()
+    msg_response = devuelto['USD']['transferencia']
+
+    response = '{0} El precio del paralelo en Vzla es: {1}'
+    bot.send_message(chat_id, response.format(user_first_name, msg_response))
+
+@bot.message_handler(commands=['hola'])
+def comando_hola(mensaje):
+    '''.'''
+
+    print(mensaje)
+    chat_id = mensaje.chat.id
+    user_first_name = mensaje.from_user.first_name
+
+    bot.send_message(chat_id, 'Que fue mijo {0} como estais..?'.format(user_first_name))
+
 @bot.message_handler(commands=['quierocolaborar'])
 def comando_quierocolaborar(mensaje):
     '''.'''
@@ -55,81 +127,23 @@ def comando_quierocolaborar(mensaje):
     response = '{0} {1}'
     bot.send_message(chat_id, response.format(user_first_name, msg_response))
 
-@bot.message_handler(commands=['ayudame'])
-def comando_ayuda(mensaje):
-    response = ayuda(mensaje)
-
-@bot.message_handler(commands=['help'])
-def comando_ayuda(mensaje):
-    response = ayuda(mensaje)
-
-@bot.message_handler(commands=['?'])
-def comando_ayuda(mensaje):
-    response = ayuda(mensaje)
-
-@bot.message_handler(commands=['dameunaayudaitaahi'])
-def comando_ayuda(mensaje):
-    response = ayuda(mensaje)
-
-def ayuda(mensaje):
-    '''.'''
-
-    chat_id = mensaje.chat.id
-    msg_response = """
-    Lista de Comandos:
-
-    /calcular  La suma de 2 mas  es [2+2] y 3 por 3 es [3*3]
-    /hola
-    /chao
-    /help
-    /dolartoday
-    /bitcoin
-    /porno
-    """
-    user_first_name = mensaje.from_user.first_name
-
-    response = 'Ey..! {0} aqui teneis tu ayuda, {1}'
-    bot.send_message(chat_id, response.format(user_first_name, msg_response))
-
-@bot.message_handler(commands=['dolartoday'])
-def comando_dolartoday(mensaje):
+@bot.message_handler(commands=['panorama'])
+def sucesos(mensaje):
     '''.'''
 
     chat_id = mensaje.chat.id
     user_first_name = mensaje.from_user.first_name
 
-    rq = requests.get('https://s3.amazonaws.com/dolartoday/data.json')
-    devuelto = rq.json()
-    msg_response = devuelto['USD']['transferencia']
+    noti = NoticiasPanorama()
+    response = noti.sucesos()
 
-    response = '{0} El precio del paralelo en Vzla es: {1}'
-    bot.send_message(chat_id, response.format(user_first_name, msg_response))
-
-@bot.message_handler(commands=['bitcoin'])
-def comando_bitcoin(mensaje):
-    '''.'''
-
-    chat_id = mensaje.chat.id
-    user_first_name = mensaje.from_user.first_name
-
-    url = "https://api.coinbase.com/v2/exchange-rates?currency=BTC"
-    response = requests.get(url).json().get("data").get("rates").get("USD")
-
-    msg_response = '{0} El precio del Bitcoin es: {1} USD'.format(user_first_name, response)
-    bot.send_message(chat_id, msg_response)
-
-@bot.message_handler(commands=['chao'])
-def comando_chao(mensaje):
-    '''.'''
-
-    chat_id = mensaje.chat.id
-    bot.send_message(chat_id, 'Dala papi nos vemos, cualquier verga gritais')
+    bot.send_message(chat_id, response)
 
 # ---------------------------------------------------------------------------------------------
 # Groserias y Jodezon
 
 @bot.message_handler(commands=['porno'])
-def comando_porno(mensaje):
+def porno(mensaje):
     '''.'''
 
     chat_id = mensaje.chat.id
@@ -142,25 +156,25 @@ def comando_porno(mensaje):
     bot.send_message(chat_id, response.format(user_first_name, msg_response))
 
 @bot.message_handler(commands=['gay'])
-def comando_chao(mensaje):
+def chao(mensaje):
     '''.'''
 
     alias_gay(mensaje)
 
 @bot.message_handler(commands=['marico'])
-def comando_chao(mensaje):
+def chao(mensaje):
     '''.'''
 
     alias_gay(mensaje)
 
 @bot.message_handler(commands=['marisco'])
-def comando_chao(mensaje):
+def chao(mensaje):
     '''.'''
 
     alias_gay(mensaje)
 
 @bot.message_handler(commands=['homosexual'])
-def comando_chao(mensaje):
+def chao(mensaje):
     '''.'''
 
     alias_gay(mensaje)
@@ -173,9 +187,8 @@ def alias_gay(mensaje):
 
     bot.send_message(chat_id, 'Mas marico sois vos {0}'.format(user_first_name))
 
-
 @bot.message_handler(commands=['mamamelo'])
-def comando_chao(mensaje):
+def chao(mensaje):
     '''.'''
 
     chat_id = mensaje.chat.id
