ea06b3753646899866c7f205ec3815a2b13a1be7
foxcarlos <foxcarlos@gmail.com>
2017-10-27 15:04:14 -0300

    modelos para alertas

--- bot/admin.py
+++ bot/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+
+from .models import Alerta, AlertaUsuario
+
+class AlertaAdmin(admin.ModelAdmin):
+    model = Alerta
+
+    list_display = ['comando', 'descripcion', 'activo', 'pk']
+    search_fields = ['comando', 'activo']
+
+
+class AlertaUsuarioAdmin(admin.ModelAdmin):
+    model = AlertaUsuario
+
+    list_display = ['alerta', 'estado', 'chat_id', 'chat_username']
+    search_fields = ['alerta', 'estado', 'chat_username']
+    list_filter = ['alerta']
+
+
+admin.site.register(Alerta, AlertaAdmin)
+admin.site.register(AlertaUsuario, AlertaUsuarioAdmin)
--- bot/models.py
+++ bot/models.py
@@ -3,3 +3,43 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Alerta(models.Model):
+    '''.'''
+    comando = models.CharField(max_length=100, blank=True, null=True, unique=True)
+    descripcion = models.TextField(blank=True, null=True)
+    activo = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.comando
+
+    class Meta:
+        '''.'''
+        verbose_name_plural = 'Alertas'
+        verbose_name = 'Alerta'
+        ordering = ['comando']
+
+
+class AlertaUsuario(models.Model):
+    '''.'''
+    alerta = models.ForeignKey(Alerta, related_name='alertas')
+    estado = models.CharField(max_length=3, default="Off",\
+            choices=(
+                ('A', 'On'),
+                ('I', 'Off')
+                ),)
+
+    chat_id = models.IntegerField(default=0)
+    chat_username = models.CharField(max_length=100, blank=True, null=True)
+
+
+    def __str__(self):
+        return self.alerta.comando
+
+    class Meta:
+        '''.'''
+        verbose_name_plural = 'AlertasUsuarios'
+        verbose_name = 'AlertaUsuario'
+        ordering = ['alerta']
+
+
