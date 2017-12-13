4a59c9e675389a1a10ab7e121b2b7f9e0e6867b5
foxcarlos <foxcarlos@gmail.com>
2017-12-13 16:29:16 -0300

    cambios en las alarmas

--- alarmas.py
+++ alarmas.py
@@ -1,24 +1,32 @@
+# from django.conf import settings
+from sampleproject import settings
+
 import os
 from time import sleep
 
+
+
 while True:
+    base = settings.BASE_DIR
+    django_mng = os.path.join(base, "manage.py")
+
     print("Ejecutando Alarma bitcoin")
-    comando = "python /home/foxcarlos/decimemijobot/manage.py alerta_bitcoin bitcoin"
+    comando = "python {} alerta_bitcoin bitcoin".format(django_mng)
     try:
         os.system(comando)
     except Exceptions as e:
         pass
     sleep(30)
     print("Ejecutando Alarma dolartoday")
-    comando2 = "python /home/foxcarlos/decimemijobot/manage.py alerta_bitcoin dolartoday"
+    comando2 = "python {0} alerta_bitcoin dolartoday".format(django_mng)
     os.system(comando2)
     sleep(30)
     print("Ejecutando Alarma Ethereum")
-    comando3 = "python /home/foxcarlos/decimemijobot/manage.py alerta_bitcoin ethereum"
+    comando3 = "python {0} alerta_bitcoin ethereum".format(django_mng)
     os.system(comando3)
     sleep(30)
     print("Ejecutando Alarma LiteCoin")
-    comando4 = "python /home/foxcarlos/decimemijobot/manage.py alerta_bitcoin litecoin"
+    comando4 = "python {0} alerta_bitcoin litecoin".format(django_mng)
     os.system(comando4)
     sleep(30)
 
