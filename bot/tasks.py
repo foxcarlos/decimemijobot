98830b7723e8f9984b8ce8c08d51d60dae3ecf41
foxcarlos <foxcarlos@gmail.com>
2018-02-15 16:19:20 -0300

    [BuG] se corrigio el error en la tarea

--- bot/tasks.py
+++ bot/tasks.py
@@ -3,6 +3,8 @@
 from django_telegrambot.apps import DjangoTelegramBot
 from sampleproject.celery import app
 from time import sleep
+from pytube import YouTube
+import os
 
 from emoji import emojize
 # celery -A pyloro worker -l info
@@ -30,29 +32,28 @@ def grupo_message(grupos, cadena_sin_el_comando):
         sleep(3)
 
 def convert_to_mp3(stream, file_handle):
+    import os
     try:
         nombre = os.path.split(os.path.abspath(file_handle.name))[1]
-        filename = nombre.replace(" ", "_")
+        print('Nombre', nombre)
+        filename = nombre.replace(" ", "_").replace("(", "").replace(")", "")
         comando = "ffmpeg -i '{0}' -vn -ar 44100 -ac 2 -ab 192 -f mp3 {1}.mp3".format(nombre, filename)
         os.system(comando)
-        msg_response = ":bell: {0} Archivo procedo con exito.".foramt(
+        msg_response = ":bell: {0} Archivo procedo con exito.".format(
                 filename)
     except Exception as E:
-        print(E)
+        print('error', E)
         msg_response = " :x: <b>Ocurrio un error al procesar el arachivo</b>"
     return msg_response
 
 
 @app.task
 def yt2mp3(chat_id, url):
-    from pytube import YouTube
-    import os
-
     yt = YouTube(url)
     msg_response = yt.register_on_complete_callback(convert_to_mp3)
     yt.streams.filter(only_audio=True).first().download()
+    print('msg_response', msg_response)
 
     # msg_response = " :x: <b>Ocurrio un error al procesar el arachivo</b>"
-    DjangoTelegramBot.dispatcher.bot.sendMessage(chat_id,
-            parse_mode="html", text=emojize(msg_response, use_aliases=True))
+    DjangoTelegramBot.dispatcher.bot.sendMessage(chat_id, parse_mode="html", text=emojize(msg_response, use_aliases=True))
 
