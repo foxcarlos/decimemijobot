55f563be672396876e618a4bf54c72c9b2ab43a7
foxcarlos <foxcarlos@gmail.com>
2018-02-16 16:54:06 -0300

    [FIX] corregido el bug de la tarea

--- bot/tasks.py
+++ bot/tasks.py
@@ -31,29 +31,35 @@ def grupo_message(grupos, cadena_sin_el_comando):
             print(E)
         sleep(3)
 
-def convert_to_mp3(stream, file_handle):
-    import os
-    try:
-        nombre = os.path.split(os.path.abspath(file_handle.name))[1]
-        print('Nombre', nombre)
-        filename = nombre.replace(" ", "_").replace("(", "").replace(")", "")
-        comando = "ffmpeg -i '{0}' -vn -ar 44100 -ac 2 -ab 192 -f mp3 {1}.mp3".format(nombre, filename)
-        os.system(comando)
-        msg_response = ":bell: {0} Archivo procedo con exito.".format(
-                filename)
-    except Exception as E:
-        print('error', E)
-        msg_response = " :x: <b>Ocurrio un error al procesar el arachivo</b>"
-    return msg_response
-
 
 @app.task
 def yt2mp3(chat_id, url):
-    yt = YouTube(url)
-    msg_response = yt.register_on_complete_callback(convert_to_mp3)
-    yt.streams.filter(only_audio=True).first().download()
-    print('msg_response', msg_response)
+    msg_response = []
+    def convert_to_mp3(stream, file_handle):
+        import os
+        try:
+            nombre = os.path.split(os.path.abspath(file_handle.name))[1]
+            print('Nombre', nombre)
+            filename = nombre.replace(" ", "_").replace("(", "").replace(")", "")
+            filename_mp3 = "{0}.mp3".format(filename)
+            comando = "ffmpeg -i '{0}' -vn -ar 44100 -ac 2 -ab 192 -f mp3 {1}".format(nombre, filename_mp3)
+            os.system(comando)
+            msg_response.append(":bell: {0} Archivo procedo con exito.".format(
+                    filename_mp3))
+        except Exception as E:
+            print('error', E)
+            msg_response.append(":x: <b>Ocurrio un error al procesar el arachivo</b>")
+        return msg_response
 
-    # msg_response = " :x: <b>Ocurrio un error al procesar el arachivo</b>"
-    DjangoTelegramBot.dispatcher.bot.sendMessage(chat_id, parse_mode="html", text=emojize(msg_response, use_aliases=True))
+    yt = YouTube(url)
+    yt.register_on_complete_callback(convert_to_mp3)
+    yt.streams.filter(only_audio=True).first().download()
+
+    file_ = open("bot/static/img/{0}".format(msg_response[0]), "rb")
+    bot.sendPhoto(update.message.chat_id,  photo=file_, caption="Si deseas haer una colaboracion de bitcoin puedes hacerlo a la siguiente direccion del wallet 1EXj4afHxArsPesqFfPwYcr522JkYrPcMq")
+
+    DjangoTelegramBot.dispatcher.bot.sendMessage(chat_id,
+            parse_mode="html", text=emojize(msg_response[0],
+                use_aliases=True)
+            )
 
