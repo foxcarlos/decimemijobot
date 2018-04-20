b7a9b2ae0361c7ebbec0e038113127ad022a1bb7
foxcarlos <foxcarlos@gmail.com>
2018-04-20 11:48:40 -0300

    [ADD] calculo del vcalor del rublo

--- bot/telegrambot.py
+++ bot/telegrambot.py
@@ -1282,8 +1282,8 @@ def get_dolartoday2():
 def get_rublo():
     data = get_price_usd_eur("btc", 'coinbase')
 
-    total_btc = 1.0 / data.get(moneda)
-    total_dolar = 1.0 / (data.get(moneda) / data.get('USD'))
+    total_btc = 1.0 / data.get('RUB')
+    total_dolar = 1.0 / (data.get('RUB') / data.get('USD'))
 
     monto, total_btc, total_dolar = valida_calcula_moneda(
             'RUB', 1, data)
